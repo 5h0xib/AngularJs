33ed3286a2c7d03b5b8f17e8a95697efeba88657
5h0xib <139106374+5h0xib@users.noreply.github.com>
2024-11-13 19:35:41 +0530

    Update app.js

--- app/app.js
+++ app/app.js
@@ -3,7 +3,7 @@ var myApp = angular.module("myApp",['ngRoute','ngAnimate']);
 // before app runs
 myApp.config(['$routeProvider', '$locationProvider', function($routeProvider,$locationProvider){
 
-    $locationProvider.html5Mode(true);
+    //$locationProvider.html5Mode(true);
 
     $routeProvider
     .when('/home', {
